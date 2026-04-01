d781ecc5191d41e2cbb4ad7a5654119c0ab722b8
andy-lov <andyhartanto483@gmail.com>
2026-04-01 14:58:43 +0700

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
-  title: 'Sink',
-  github: 'https://github.com/miantiao-me/sink',
-  coffee: 'https://sink.cool/coffee',
-  twitter: 'https://sink.cool/x',
-  telegram: 'https://sink.cool/telegram',
+  title: 'Blyn',
+  github: 'https://github.com/Blyn-hub/',
+  coffee: 'https://blyn.link/coffee',
+  twitter: 'https://blyn.link/twitter',
+  telegram: 'https://blyn.link/telegram',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  image: 'https://blyn.link/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
